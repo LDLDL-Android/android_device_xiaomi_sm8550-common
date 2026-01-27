3265cb64f717bfda0ae59d09b0098f49423b12dc
LDLDL <hyx1278199910@outlook.com>
2026-01-27 17:04:48 +0800

    Fix camera

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,6 +170,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SEPolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 # System properties
